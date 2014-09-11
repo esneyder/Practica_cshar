8ea9ecb2358314a97cbb5609d50d9b948612f9bb
esneyder <edesalla17@gmail.com>
2014-09-11 04:27:28 -0500

    método para insertar en la base de datos

--- Negocio/Persona.cs
+++ Negocio/Persona.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Datos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,13 +9,30 @@ namespace Negocio
 {
     public class Persona
     {
-        public string cedula { get;set ;}
-        public string nombre{ get; set; }
+        public string cedula { get; set; }
+        public string nombre { get; set; }
         public string apellido { get; set; }
         public DateTime fechaNacimiento { get; set; }
         public int edad { get; set; }
         public string ciudad { get; set; }
 
-       
+        public int nuevaPersona(Persona persona)
+        {
+            DatosSistema datos = new DatosSistema();
+            string[] parametros = {"@operacion",
+                                      "@cedula",
+                                      "@nombre",
+                                      "@apellido",
+                                      "@fechaNacimiento",
+                                      "@edad",
+                                      "@ciudad"};
+            return datos.Ejecutar("spPersonaIA", parametros, "I", persona.cedula,
+                                persona.nombre,
+                                persona.apellido,
+                                persona.fechaNacimiento,
+                                persona.edad,
+                                persona.ciudad);
+
+        }
     }
 }
